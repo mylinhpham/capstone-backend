518129187eb0040c74fb6602b89bb1177a074978
mylinhpham <myylinhpham@gmail.com>
2021-04-22 10:45:25 -0700

    Added more constants to db

--- src/main/resources/sqlQueries/query_1.sql
+++ src/main/resources/sqlQueries/query_1.sql
@@ -25,3 +25,18 @@ insert  into constant(string_name, string_value, char_limit, url)values("COLON_C
 insert  into constant(string_name, string_value, char_limit, url)values("PATIENT_STORIES", 'Patient Stories', 250, "");
 insert  into constant(string_name, string_value, char_limit, url)values("MEET_TEAM", 'Meet Your Team', 250, "");
 insert  into constant(string_name, string_value, char_limit, url)values("ABOUT", 'About the Site', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("CONSULTATION_HEADER_TEXT" , 'Consultation', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("CONSULTATION_HEADER_SUBTEXT" , 'Discussing Your Treatment', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("FIRST_APPOINTMENT" , 'Your First Appointment', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("FIRST_APPOINTMENT_TEXT" , 'So you are having radiation treatment. Right now there is a lot of information to understand. You might feel overwhelmed, that is okay. In the following sections we hope to show you what you can expect in the days and months to come.', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("GETTING_TO_APPOINTMENT" , 'Getting to Your Appointment', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("GETTING_TO_APPOINTMENT_TEXT" , 'Navigating a radiology clinic for the first time can be overwhelming and, while you will have a staff of nurses and medical professionals to guide you, with our Virtual Clinic Walkthrough you can get a full tour of the facility before ever stepping foot into the clinic. ', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("TEAM_TODAY" , 'Meet The Team', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("TEAM_TODAY_TEXT" , 'Working by your side during your treatment will be your dedicated care team, a group of medical experts who will do their upmost to support you. Today you might even meet with a few of them. ', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("TOPIC_TO_ADDRESS" , 'Topics to Address ', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("TOPIC_TO_ADDRESS_TEXT" , 'You might have a few questions on your first day, that is expected, and perfectly fine. Feel free to use the Interactive FAQ below to get some of those questions answered and receive ideas of topics to address both today and throughout your treatment.', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("PATIENT_STORIES_TEXT" , 'Today might be your first day of your treatment journey but thousands of others patients have come before you. Before proceeding to the next section hear what just some of them had to say about their first day and receive encouragement about the journey that lies in front of you.', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("BREASTCANCER_VIDEO_1" , 'sBM5R6mQ_EE', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("BREASTCANCER_VIDEO_2" , 'XFpA9xIOnyM', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("BREASTCANCER_VIDEO_3" , 'ccl1dQZKqto', 250, "");
+insert  into constant(string_name, string_value, char_limit, url)values("BREASTCANCER_VIDEO_4" , 'X8u0vOSCa_E', 250, "");
